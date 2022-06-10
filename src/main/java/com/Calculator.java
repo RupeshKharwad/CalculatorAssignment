ec134b01d0bce640707f2b5e715b00dd4d5a7866
RupeshKharwad <rupesh.rajput005@gmail.com>
2022-06-10 10:58:34 +0530

    second commit for multiply method

--- src/main/java/com/Calculator.java
+++ src/main/java/com/Calculator.java
@@ -9,8 +9,13 @@ public class Calculator {
 		return a-b;
 	}
 	
+	public static int multiply(int a, int b) {
+		return a*b;
+	}
+	
 	public static void main(String args[]) {
 		System.out.println("Sum of 10 and 20 is"+add(10,20));
 		System.out.println("Subtraction of 10 and 20 is"+subtract(10,20));
+		System.out.println("Subtraction of 10 and 20 is"+multiply(10, 20));
 	}
 }
